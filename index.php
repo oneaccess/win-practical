df9acd6f440dfc9ad200f2a5ef14e6b957b52e18
oneaccess <cisarese@hotmail.com>
2017-09-18 16:18:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>How to Create Webpage or Website with SliceMaker Products?</title>
+<meta name="keywords" content="create webpage, create webpage, how to create webpage, how to create website, slicemaker tutorial" />
+<meta name="description" content="This webpage is created by SliceMaker products. You can easily create wonderful webpage with the help of SliceMaker products." />
+
+<link rel="stylesheet" type="text/css" href="css/example.css" />
+
+<!--[if lte IE 6]>
+<script type="text/javascript" src="js/DD_belatedPNG_0.0.8a.js"></script>
+<script type="text/javascript">
+  DD_belatedPNG.fix('*');
+</script>
+<![endif]-->
+
+<script type="text/javascript" src="js/gl_function.js"></script>
+
+
+</head>
+<body>
+<form action="" method="post" onsubmit=''>
+  <center>
+         <div id="example_1">
+          <div id="example_6">
+               <div id="example_7"><a href='http://www.slicemaker.com' target='_self' title='Visit Our Homepage - SliceMaker'><img id='img_7' src='images/example/example_7.png' width='264' height='55' border='0' /></a></div>
+               <div id="example_12">               
+                    <div class="example_12_captions">
+                      <div class='example_12_caption_pre'></div>
+                      <div id='example_12_caption_0' class='example_12_0_caption_now'  onmouseover="gl_setTab('example_12',5,0);"  onclick="gl_setTabSelect('example_12',0)" ><a href='http://www.slicemaker.com' target='_blank'>Home</a>
+                      </div>
+                      <div class='example_12_caption_space'></div>
+                      <div id='example_12_caption_1' class='example_12_1_caption_normal'  onmouseover="gl_setTab('example_12',5,1);"  onclick="gl_setTabSelect('example_12',1)"><a href='http://www.slicemaker.com' target='_blank'>Services</a>
+                      </div>
+                      <div class='example_12_caption_space'></div>
+                      <div id='example_12_caption_2' class='example_12_2_caption_normal'  onmouseover="gl_setTab('example_12',5,2);"  onclick="gl_setTabSelect('example_12',2)"><a href='http://www.slicemaker.com' target='_blank'>News</a>
+                      </div>
+                      <div class='example_12_caption_space'></div>
+                      <div id='example_12_caption_3' class='example_12_3_caption_normal'  onmouseover="gl_setTab('example_12',5,3);"  onclick="gl_setTabSelect('example_12',3)"><a href='http://www.slicemaker.com' target='_blank'>Support</a>
+                      </div>
+                      <div class='example_12_caption_space'></div>
+                      <div id='example_12_caption_4' class='example_12_4_caption_normal'  onmouseover="gl_setTab('example_12',5,4);"  onclick="gl_setTabSelect('example_12',4)"><a href='http://www.slicemaker.com' target='_blank'>Company</a>
+                      </div>
+                      <div class='example_12_caption_last'></div>
+                    </div>
+                    <div class="example_12_contents">
+                      <div id='example_12_content_0' style=''>
+                      </div>
+                      <div id='example_12_content_1' style='display:none;' >
+                      </div>
+                      <div id='example_12_content_2' style='display:none;' >
+                      </div>
+                      <div id='example_12_content_3' style='display:none;' >
+                      </div>
+                      <div id='example_12_content_4' style='display:none;' >
+                      </div>
+                    </div>
+                    <script>gl_setTabNowUrl('example_12',5)</script>
+
+               </div>
+          </div>
+     </div>
+     <div id="example_2">
+          <div id="example_13">
+            <object type="application/x-shockwave-flash" data="bcastr4.swf?xml=get_xml_13.xml"  width="1302" height="400" id="vcastr13">
+          	    <param name="movie" value="bcastr4.swf?xml=get_xml_13.xml" />
+                <param name="wmode" value="transparent">
+            </object>
+          </div>
+     </div>
+     <div id="example_3">
+          <div id="example_14">          
+               <div class="example_14_captions">
+                 <div class='example_14_caption_pre'></div>
+                 <div id='example_14_caption_0' class='example_14_0_caption_now'  onmouseover="gl_setTab('example_14',3,0);"  onclick="gl_setTabSelect('example_14',0)" >Our Services
+                 </div>
+                 <div class='example_14_caption_space'></div>
+                 <div id='example_14_caption_1' class='example_14_1_caption_normal'  onmouseover="gl_setTab('example_14',3,1);"  onclick="gl_setTabSelect('example_14',1)">Contact Us
+                 </div>
+                 <div class='example_14_caption_space'></div>
+                 <div id='example_14_caption_2' class='example_14_2_caption_normal'  onmouseover="gl_setTab('example_14',3,2);"  onclick="gl_setTabSelect('example_14',2)">Create Website
+                 </div>
+                 <div class='example_14_caption_last'></div>
+               </div>
+               <div class="example_14_contents">
+                 <div id='example_14_content_0' style=''>
+                      <div id="example_15">
+                           <div id="example_16">
+                                <div id="example_17"><h2>What Can We Do For You?</h2>
+I am just a pragraph here, you can input your own contents here. You can easily input contents here through SliceMaker products. I am just a pragraph here, you can input your own contents here. You can easily input contents here through SliceMaker products. I am just a pragraph here, you can input your own contents here. You can easily input contents here through SliceMaker products. <br /></div>
+                                <div id="example_18"><img id='img_18' src='images/example/sliding-1.png' width='478' height='254' border='0'  /></div>
+                           </div>
+                      </div>
+                 </div>
+                 <div id='example_14_content_1' style='display:none;' >
+                      <div id="example_15">
+                           <div id="example_16">
+                                <div id="example_17"><h2>Contact Us</h2>
+Contact our support team if you have probelms when using SliceLMaker products. I am just a pragraph here, you can input your own contents here. I am just a paragraph here, you can input your won contents here. I am just a paragraph here, you can input your own contents here. I am just a paragraph here, you can input your own contents here.<br /></div>
+                                <div id="example_18"><img id='img_18' src='images/example/sliding-2.png' width='478' height='254' border='0'  /></div>
+                           </div>
+                      </div>
+                 </div>
+                 <div id='example_14_content_2' style='display:none;' >
+                      <div id="example_15">
+                           <div id="example_16">
+                                <div id="example_17"><h2>Create Website</h2>
+If you want to create this kind of webpage or website, you can choose SliceMaker Platinum or SliceMaker Deluxe version. It is very easy to create a webpage or a website with the help of SliceMaker products. And, we promise to our users that we will deal with whatever problem they have within 24 hours. I am just a pragraph here, you can input your own contents here. I am just a paragraph here, you can input your won contents here. I am just&nbsp;a paragraph here, you can input your own contents here. I am just a paragraph here, you can input your own contents here.<br /></div>
+                                <div id="example_18"><img id='img_18' src='images/example/example_18.png' width='478' height='254' border='0'  /></div>
+                           </div>
+                      </div>
+                 </div>
+               </div>
+               <script>gl_setTabNowUrl('example_14',3)</script>
+
+          </div>
+     </div>
+     <div id="example_4">
+	 <div id="plus_1"> 
+          <div id="example_27"><img id='img_27' src='images/example/example_27.png' width='444' height='296' border='0'  /></div>
+          <div id="example_28"><h2>Making Webpage Easily</h2>
+SliceMaker products let you create webpage/website in an ituitive way. You can create create exception webpage/website within several simple clicks. Now, it is time for you to create a webpage/website on your own.<br></div>
+</div>
+     </div>
+     <div id="example_5">
+	<div id="plus_2">
+          <div id="plus_3"><a href="http://www.slicemaker.com">SliceMaker</a></div>
+          <div id="plus_4">
+          </div>
+          <div id="plus_5"><a href="http://www.slicemaker.com/inner_171.htm">Privacy Policy</a></div>
+           <div id="plus_4">
+          </div>
+          <div id="plus_7"><a href="http://www.slicemaker.com/inner_150.htm">Contact</a></div>
+           <div id="plus_4">
+          </div>
+          <div id="plus_9"><a href="http://www.slicemaker.com/buy_center.htm">Buy Now</a></div>
+           <div id="plus_4">
+          </div>
+          <div id="plus_11"> © 2010 - 2013 SliceMaker Soft. All Rights Reserved</div>
+          <div id="plus_12"><a href="http://www.facebook.com/SlicemakerSoft"><img src='images/example/plus_12.png' name="img_12" width='39' height='35' border='0' id='img_12'  /></a></div>
+          <div id="plus_13"><a href="https://twitter.com/slicemaker"><img src='images/example/plus_13.png' name="img_13" width='44' height='35' border='0' id='img_13'  /></a></div>
+     </div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</div>
+	 
+	 
+	 
+	 
+	 
+	 
+     </div>
+
+  </center>
+</form>
+</body>
+</html>
