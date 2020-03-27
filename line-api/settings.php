ebd9b9ed715b32bc13f1bbda8e217d398c2d589d
oneaccess <cisarese@hotmail.com>
2020-03-27 16:48:49 +0700

    Update settings.php

--- line-api/settings.php
+++ line-api/settings.php
@@ -1,10 +1,9 @@
 <?PHP
+    
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1502306338');
+define('LINE_MESSAGE_CHANNEL_SECRET','800277791d946cc4e2847fbe2b48578e');
+define('LINE_MESSAGE_ACCESS_TOKEN','vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=');
 
-return [
-    'channelId' => '1502306338',
-    'channelSecret' => '800277791d946cc4e2847fbe2b48578e',
-    'channelMid' => 'ucf3a817b00c639d97ec3ace02f68fc6b',
-    'targetMid' => ['ufce3d04785caf9f48fc79dd3e0256200'],
-    'accessToken' => 'vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=';
-
+    
 ?>
