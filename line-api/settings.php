0b0051e681bc2f5ea1414ccb0d873ada72dfee63
oneaccess <cisarese@hotmail.com>
2020-03-27 16:33:51 +0700

    Add files via upload

---
+++ line-api/settings.php
@@ -0,0 +1,11 @@
+<?PHP
+
+return [
+    'channelId' => '1485920325',
+    'channelSecret' => '81c7144d878de9dc9c041142c1d243af',
+    'channelMid' => 'ucf3a817b00c639d97ec3ace02f68fc6b',
+    'targetMid' => ['ufce3d04785caf9f48fc79dd3e0256200'],
+    'accessToken' => 'bjMobsS6ndui8SI7L3yfrJTH/J70rI8N/UdD2xX6vKOJQgbooymVfPIiDMOer8HbfO4rhEtXypseVhC9nY4HoRIUnZQ3j0CGVFwLc8Pq/a4lddesAE/PjK06ihIXeq+rw/gWf+5jD/Lf8ToVJPvNuwdB04t89/1O/w1cDnyilFU=',
+];
+
+?>
