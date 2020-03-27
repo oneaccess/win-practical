ce913928d7d6596b7dc37524a6b99a985bf153b7
oneaccess <cisarese@hotmail.com>
2020-03-27 13:23:59 +0700

    Update verify.php

--- line-api/verify.php
+++ line-api/verify.php
@@ -1,5 +1,5 @@
 <?php 
-  $access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+  $access_token = '5PEITvKLXM9jp/t7iNs+U56eq4tV+kp13AuveYhh9Kj36A41neuftxaScKfrTfFWuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQt35BfZP3b5LjJGFHK1hxXURsZuv7e+smCmQP6nI0pwgdB04t89/1O/w1cDnyilFU=';
   $url = 'https://api.line.me/v1/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
