60f5847e1afc1407c5c400c9f91cb528fd7a62aa
oneaccess <cisarese@hotmail.com>
2020-03-27 14:43:03 +0700

    Update verify.php

--- line-api/verify.php
+++ line-api/verify.php
@@ -1,10 +1,38 @@
 <?php 
-  $access_token = '5PEITvKLXM9jp/t7iNs+U56eq4tV+kp13AuveYhh9Kj36A41neuftxaScKfrTfFWuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQt35BfZP3b5LjJGFHK1hxXURsZuv7e+smCmQP6nI0pwgdB04t89/1O/w1cDnyilFU=';
-  $url = 'https://api.line.me/v1/oauth/verify';
-  $headers = array('Authorization: Bearer ' . $access_token);
-  $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  $result = curl_exec($ch);curl_close($ch);
-  echo $result;
+  $userId = 'Uf34420370c875a3540bd79f148c915c2';
+  $access_token = 'vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=';
+function getLINEProfile($datas)
+{
+   $datasReturn = [];
+   $curl = curl_init();
+   curl_setopt_array($curl, array(
+     CURLOPT_URL => $datas['url'],
+     CURLOPT_RETURNTRANSFER => true,
+     CURLOPT_ENCODING => "",
+     CURLOPT_MAXREDIRS => 10,
+     CURLOPT_TIMEOUT => 30,
+     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+     CURLOPT_CUSTOMREQUEST => "GET",
+     CURLOPT_HTTPHEADER => array(
+       "Authorization: Bearer ".$datas['token'],
+       "cache-control: no-cache"
+     ),
+   ));
+   $response = curl_exec($curl);
+   $err = curl_error($curl);
+   curl_close($curl);
+   if($err){
+      $datasReturn['result'] = 'E';
+      $datasReturn['message'] = $err;
+   }else{
+      if($response == "{}"){
+          $datasReturn['result'] = 'S';
+          $datasReturn['message'] = 'Success';
+      }else{
+          $datasReturn['result'] = 'E';
+          $datasReturn['message'] = $response;
+      }
+   }
+   return $datasReturn;
+}
 ?>
