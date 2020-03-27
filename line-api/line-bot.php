ba136d0434961529838b83b0c0c6e4fba4b15f4a
oneaccess <cisarese@hotmail.com>
2020-03-27 15:02:50 +0700

    Update line-bot.php

--- line-api/line-bot.php
+++ line-api/line-bot.php
@@ -10,8 +10,8 @@
 
 	$messages = [];
 	$messages['replyToken'] = $replyToken;
-	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
-
+	//$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
+        $messages['messages'][0] = getFormatTextMessage($deCode);
 	$encodeJson = json_encode($messages);
 
 	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
