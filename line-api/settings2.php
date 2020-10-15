c5b936ba5938618eef3279cc51650db6551eb876
oneaccess <cisarese@hotmail.com>
2020-10-15 10:55:03 +0700

    Create settings2.php

---
+++ line-api/settings2.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'channelId' => '1502306338',
+    'channelSecret' => '800277791d946cc4e2847fbe2b48578e',
+    'channelMid' => 'u44910ea32c9d6a1457be25b056c01142',
+    'targetMid' => ['U8ce89a9c5318ef4fbbeef105ca1bf201'],
+    'accessToken' => '5PEITvKLXM9jp/t7iNs+U56eq4tV+kp13AuveYhh9Kj36A41neuftxaScKfrTfFWuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQt35BfZP3b5LjJGFHK1hxXURsZuv7e+smCmQP6nI0pwgdB04t89/1O/w1cDnyilFU=',
+];
+
+?>
