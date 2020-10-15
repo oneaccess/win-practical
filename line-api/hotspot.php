c36812418b7c414f5f0e1f5b1cc62a0d196741b1
oneaccess <cisarese@hotmail.com>
2020-10-15 12:40:12 +0700

    Update hotspot.php

--- line-api/hotspot.php
+++ line-api/hotspot.php
@@ -38,7 +38,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $msg.class_exists('LINE\LINEBot'),	//$text
+				'text' => $msg.print_r(class_exists('LINE\LINEBot'),true),	//$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
