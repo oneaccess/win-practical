bc0dbd68afedaa3a2aa100ff6b65d12ac4a2e666
oneaccess <cisarese@hotmail.com>
2020-10-15 10:58:35 +0700

    Update settings2.php

--- line-api/settings2.php
+++ line-api/settings2.php
@@ -4,7 +4,7 @@ return [
     'channelSecret' => '800277791d946cc4e2847fbe2b48578e',
     'channelMid' => 'u44910ea32c9d6a1457be25b056c01142',
     'targetMid' => ['U8ce89a9c5318ef4fbbeef105ca1bf201'],
-    'accessToken' => '5PEITvKLXM9jp/t7iNs+U56eq4tV+kp13AuveYhh9Kj36A41neuftxaScKfrTfFWuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQt35BfZP3b5LjJGFHK1hxXURsZuv7e+smCmQP6nI0pwgdB04t89/1O/w1cDnyilFU=',
+    'accessToken' => 'vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=',
 ];
 
 ?>
