9e530125a540ede6f3485d1a18aa9791f988ecf3
oneaccess <cisarese@hotmail.com>
2020-10-15 11:26:49 +0700

    Update hotspot.php

--- line-api/hotspot.php
+++ line-api/hotspot.php
@@ -1,42 +1,11 @@
 <?php
 
-///////////// ส่วนของการเรียกใช้งาน class ผ่าน namespace
+
 use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-//use LINE\LINEBot\Event;
-//use LINE\LINEBot\Event\BaseEvent;
-//use LINE\LINEBot\Event\MessageEvent;
-use LINE\LINEBot\MessageBuilder;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
-use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
-use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
-use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
-use LINE\LINEBot\ImagemapActionBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
-use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
-use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
-use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
-use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
-use LINE\LINEBot\TemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
-//use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
-//use LINE\LINEBot\Message\MultipleMessages;
-//use LINE\LINEBot\Message\RichMessage\Markup;
+use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
+use LINE\LINEBot\Message\MultipleMessages;
+use LINE\LINEBot\Message\RichMessage\Markup;
 
 require_once 'vendor/autoload.php';
 
@@ -44,6 +13,7 @@ $setting = require('settings2.php');
 $channelId = $setting['channelId'];
 $channelSecret = $setting['channelSecret'];
 $accessToken = $setting['accessToken'];
+
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -60,175 +30,178 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			if ($text == 'HELP') {
-				$msg  = "CHECK คือ การตรวจสอบสถานะการใช้งานรับข้อมูล Line ของท่าน" ."\n" ."REGISTER คือ การยืนยันลงทะเบียนเพื่อใช้งานรการับข้อมูลผ่าน Line โดยการพิม REGISTER:<verify code>";
+				$msg  = "1. ส่วนข้อมูล
+	1.1 ข้อมูลนักกีฬา e-sport
+	1.2 ข้อมูลทีม e-sport
+3.หาทีมซ้อม
+	3.1 ทีมที่พร้อมจะซ้อม ณ ตอนนี้
+	3.2 ทีมที่ต้องการนัดซ้อม ตามเวลาที่กำหนด
+4.Ranking
+	4.2 Ranking Team อันดับของทีม e-sport
+ถ้าต้องการใช้งานโปรดเข้าสู่เว็บไซต์	http://honesportranking.dynu.com:60080";
 			} else if ($text == 'CHECK') {
 				$msg = check_info($event['source']['userId']);	
 				exit();
-			//} else if (preg_match('/^REGISTER\:[A-Z0-9\-]{4,8}\:.+/', $text)) {
-			} else if (preg_match('/^REGISTER\:.+/', $text)) {
-				$info = 	explode(":",$text);
-				$verify_id = $info[1];
-				/*$objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
+			} else if (preg_match('/^REGISTER\:[A-Z0-9\-]{4,8}\:.+/', $text)) {
+				$info = explode(":",$text);
+				$member_id = $info[1];
+				$line_id = $info[2];
+				$objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
 				mysqli_set_charset($objConnect, "utf8");
-				mysqli_select_db($objConnect,"svgarena");		
-				$strSQL = "SELECT member_id FROM otp_expiry WHERE votp = '$verify_id'";
+				mysqli_select_db($objConnect,"project");		
+				$strSQL = "SELECT member_id,th_name,th_surname FROM member WHERE member_id = '$member_id'";
 				$objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());
-				$no = mysqli_num_rows($objQuery);
-				if ($no > 0) {
+				$no = mysqli_num_rows($objQuery);				
+				if ($no > 0) {					
 					while($obResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC)) {
-						if (isset($obResult['member_id'])) {							
-							$member_id = $obResult['member_id'];
-							$strSQL2 = "SELECT member_id,firstname,lastname FROM users WHERE member_id = '$member_id'";
-							$objQuery2 = mysqli_query($objConnect,$strSQL2) or die (mysql_error());
-							$no2 = mysqli_num_rows($objQuery2);
-							while($obResult2 = mysqli_fetch_array($objQuery2,MYSQLI_ASSOC)) {
-							    $res_info = $obResult["member_id"]." ".$obResult2["firstname"]." ".$obResult2["lastname"];
-							}	
+						if (isset($obResult['member_id'])) {
+							$res_info = $obResult["member_id"]." ".$obResult["th_name"]." ".$obResult["th_surname"];
 						}
 					} 
-					sleep(1);
+					sleep(1);						
 					register_form($event['source']['userId'],$res_info,$member_id,$line_id);
 				    mysqli_close($objConnect); 	
-				} else {*/			
+				} else {			
 					$msg = 'ไม่พบข้อมูลรหัสสมาชิกของท่าน';
 					sleep(1);
 					reply_msg($replyToken,$msg);	
-				//}
+				}
 				exit();			
 		    	} else {
-				$msg = "ท่านกรอกข้อมูลไม่ถูกต้อง โปรดตรวจสอบความถูกต้อง หรือ พิม 'HELP'เพื่อดูวิธีการใช้งาน".$verify_id;
+				$msg = "ท่านกรอกข้อมูลไม่ถูกต้อง โปรดตรวจสอบความถูกต้อง หรือ พิม 'HELP'เพื่อดูวิธีการใช้งาน";
 			}
 			sleep(1);
 			reply_msg($replyToken,$msg);	
 		} else if ($event['type'] == 'postback') {
 			$text = $event['postback']['data'];
 			$replyToken = $event['replyToken'];
-			if (preg_match('/^#123confirm\:+/', $text)) {
+			if (preg_match('/^#123confirm\:[A-Z0-9\-]{4,8}.\:+/', $text)) {
 				$mid = $event['source']['userId'];
 				$info = 	explode(":",$text);
-				$member_id = $info[1];					
-				/*$objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
+				$member_id = $info[1];
+				$line_id = $info[2];	
+				$objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
 				mysqli_set_charset($objConnect, "utf8");
-				mysqli_select_db($objConnect,"svgarena");		
-				$strSQL = "UPDATE users SET mid = '$mid' WHERE member_id = '$member_id'";
+				mysqli_select_db($objConnect,"project");		
+				$strSQL = "UPDATE member SET mid = '$mid', line_id = '$line_id' WHERE member_id = '$member_id'";
 				$objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());
 				$no = mysqli_num_rows($objQuery);					
 				if ($no > 0) {
-					$msg = 'Line ID ของท่านลงทะเบียนกับรหัสพนักงาน'.' '.$staff_id.' '.'สามารถใช้งานได้แล้ว';					
+					$msg = 'Line ID ของท่านลงทะเบียนกับรหัสสมาชิก'.' '.$member_id.' '.'สามารถใช้งานได้แล้ว';					
 				} else {//if (isset($chk_res_info){
 					$msg = 'รหัสสมาชิกของท่านมีการลงทะเบียนใช้งานอยู่ในระบบแล้ว';
 				} 
 				mysqli_close($objConnect);
 				sleep(1);	
 				reply_msg($replyToken,$msg);
-			} else {*/
+			} else {
 				$msg = 'การลงทะเบียนของท่านถูกยกเลิกแล้ว ขอบคุณที่ใช้บริการ';
 				sleep(1);
 				reply_msg($replyToken,$msg);
 			}						
 		}								
-	}		
+	}			
 }
 
 function  reply_msg($replyToken,$msg){
-	$setting = require('/var/www/html/bom/line/settings.php');
+        $setting = require('/var/www/html/bom/line/settings.php');
         $channelId = $setting['channelId'];
         $channelSecret = $setting['channelSecret'];
         $channelMid = $setting['channelMid'];
         $accessToken = $setting['accessToken'];
-	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
-	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
-	$response = $bot->replyMessage($replyToken, $textMessageBuilder);
-	$res_status = $response->getHTTPStatus() . ' ' . $response->getRawBody();
-	return $msg;
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+        $res_status = $response->getHTTPStatus() . ' ' . $response->getRawBody();
+        return $msg;
 }
 
-/*function  register_form($mid,$msg,$member_id,$line_id){
-	$setting = require('/var/www/html/bom/line/settings.php');
+function  register_form($mid,$msg,$member_id,$line_id){
+        $setting = require('/var/www/html/bom/line/settings.php');
         $channelId = $setting['channelId'];
         $channelSecret = $setting['channelSecret'];
         $channelMid = $setting['channelMid'];
         $accessToken = $setting['accessToken'];
-	$confirm_msg = '#123confirm:'.$member_id.':'.$line_id;
-	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
-	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-	//$image_url = 'https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg';
-	$image_url = 'https://www.9tana.com/wp-content/uploads/2016/03/3bb-620x270.jpg';
-	$actBuilderd = array();
-	//$actBuilder[] = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('Yes','ระบบทำการลงทะเบียนให้ท่านแล้ว');
-	//$actBuilder[] = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('No','ขอบคุณที่ใช้บริการ');
+        $confirm_msg = '#123confirm:'.$member_id.':'.$line_id;
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+        $image_url = 'https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg';
+        $actBuilderd = array();
 	$actBuilder[] = new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('YES',$confirm_msg);
-	$actBuilder[] = new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('NO','#123cancle');
-	$templateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder('คุณต้องการลงทะเบียนกับข้อมูลดังต่อไปนี้หรือไม่',$msg, $image_url, $actBuilder);
-	$builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('ลงทะเบียนใช้งาน', $templateBuilder);
+        $actBuilder[] = new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('NO','#123cancle');
+        $templateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder('คุณต้องการลงทะเบียนกับข้อมูลดังต่อไปนี้หรือไม่',$msg, $image_url, $actBuilder);
+        $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('ลงทะเบียนใช้งาน',$templateBuilder);
 	$response = $bot->pushMessage($mid, $builder);
-	return $msg;
+        return $msg;
 }
 
-function  register($mid,$line_id,$member_id){	
-	/*$objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
-    	mysqli_select_db($objConnect,"issue_nw");		
-	$strSQL = "UPDATE member SET mid = '$mid',line_id = '$line_id' WHERE member_id = '$member_id'";
-    	$objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());
-    	$no = mysqli_num_rows($objQuery);
-	if ($no > 0) {
-		$strSQL = "";
-		$objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());	
-		while($obResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC)) {
-		
-		}
-	}*/
-}*/
+function  register($mid,$line_id,$member_id){
+        $objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
+        mysqli_select_db($objConnect,"issue_nw");
+        $strSQL = "UPDATE member SET mid = '$mid',line_id = '$line_id' WHERE member_id = '$member_id'";
+        $objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());
+        $no = mysqli_num_rows($objQuery);
+        if ($no > 0) {
+                $strSQL = "";
+                $objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());
+                while($obResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC)) {
+
+                }
+        }
+}
 
 function  check_info($mid){
-	$msg = 'ERROR';
-	$setting = require('/var/www/html/bom/line/settings.php');
-	$channelId = $setting['channelId'];
-	$channelSecret = $setting['channelSecret'];
-	$channelMid = $setting['channelMid'];
-	$accessToken = $setting['accessToken'];
+        $msg = 'ERROR';
+        $setting = require('/var/www/html/bom/line/settings.php');
+        $channelId = $setting['channelId'];
+        $channelSecret = $setting['channelSecret'];
+        $channelMid = $setting['channelMid'];
+        $accessToken = $setting['accessToken'];
 
-	/*$objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
-	mysqli_set_charset($objConnect, "utf8");
-	mysqli_select_db($objConnect,"svgarena");		
-	$strSQL = "SELECT member_id FROM users WHERE mid = '$mid'";
-	$objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());
-	$no = mysqli_num_rows($objQuery);
-	if ($no > 0) {
-		while($obResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC)) {
-			$member_id = $obResult['member_id'];
+        $objConnect = mysqli_connect("10.10.19.138","webbom","bombom") or die(mysql_error());
+        mysqli_set_charset($objConnect, "utf8");
+        mysqli_select_db($objConnect,"svgarena");
+        $strSQL = "SELECT member_id FROM users WHERE mid = '$mid'";
+        $objQuery = mysqli_query($objConnect,$strSQL) or die (mysql_error());
+        $no = mysqli_num_rows($objQuery);
+        if ($no > 0) {
+                while($obResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC)) {
+                        $member_id = $obResult['member_id'];
 			$res_msg = 'IDของท่านได้ทำการลงทะเบียนใช้งานแล้ว';
 			$strSQL2 = "SELECT member_id,firstname,lastname FROM users WHERE member_id = '$member_id'";
-			$objQuery2 = mysqli_query($objConnect,$strSQL2) or die (mysql_error());
-			$no2 = mysqli_num_rows($objQuery2);
-			while($obResult2 = mysqli_fetch_array($objQuery2,MYSQLI_ASSOC)) {
-				if (isset($obResult2['member_id'])) {
-					$res_info = $obResult2["member_id"]." ".$obResult2["firstname"]." ".$obResult2["lastname"];
-				}
-			}
-			$msg = "$res_info\n"."$res_msg";
-			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
-			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
-			$response = $bot->pushMessage($mid, $textMessageBuilder);
-		}
-	} else {*/	
-		$msg = "ท่านยังไม่ได้ทำการลงทะเบียนการใช้งานโปรดทำการลงทะเบียนโดยการพิม REGISTER:<รหัสพนักงานตัวพิมใหญ่>:<Line ID>";
-		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
-		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
-		$response = $bot->pushMessage($mid, $textMessageBuilder);
-	//}
+                        $objQuery2 = mysqli_query($objConnect,$strSQL2) or die (mysql_error());
+                        $no2 = mysqli_num_rows($objQuery2);
+                        while($obResult2 = mysqli_fetch_array($objQuery2,MYSQLI_ASSOC)) {
+                                if (isset($obResult2['member_id'])) {
+                                        $res_info = $obResult2["member_id"]." ".$obResult2["th_name"]." ".$obResult2["th_surname"];
+                                }
+                        }
+                        $msg = "$res_info\n"."$res_msg"."$mid";
+                        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+                        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+                        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+                        $response = $bot->pushMessage($mid, $textMessageBuilder);
+                }
+        } else {
+		$msg = "ท่านยังไม่ได้ทำการลงทะเบียนการใช้งานโปรดทำการลงทะเบียนโดยการพิม REGISTER:<รหัสสมาชิกตัวพิมใหญ่>:<Line ID>";
+		 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+                $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+                $response = $bot->pushMessage($mid, $textMessageBuilder);
+        }
 
-	//mysqli_close($objConnect);
-	return $msg;
+        mysqli_close($objConnect);
+        return $msg;
 }
 
-/*function write_file($txt){
-	$myfile = fopen("log.txt", "w") or die("Unable to open file!");
-	fwrite($myfile, $txt);
-	fclose($myfile);
-}*/
+function write_file($txt){
+        $myfile = fopen("log.txt", "w") or die("Unable to open file!");
+        fwrite($myfile, $txt);
+        fclose($myfile);
+}
+
+
+echo "OK";
 ?>
 
