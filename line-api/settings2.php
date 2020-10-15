63e74f70ef0d996a010ee551bddd4281455babc6
oneaccess <cisarese@hotmail.com>
2020-10-15 11:23:35 +0700

    Update settings2.php

--- line-api/settings2.php
+++ line-api/settings2.php
@@ -3,7 +3,7 @@ return [
     'channelId' => '1502306338',
     'channelSecret' => '800277791d946cc4e2847fbe2b48578e',
     'channelMid' => 'u44910ea32c9d6a1457be25b056c01142',
-    'targetMid' => ['U8ce89a9c5318ef4fbbeef105ca1bf201'],
+    'targetMid' => ['Uf34420370c875a3540bd79f148c915c2'],
     'accessToken' => 'vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=',
 ];
 
