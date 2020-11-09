86f6cd17bf56c71bab5c47cc4bdb0b0acb32f4a2
oneaccess <cisarese@hotmail.com>
2020-11-09 13:23:28 +0700

    Update hotspot.php

--- line-api/hotspot.php
+++ line-api/hotspot.php
@@ -1,6 +1,6 @@
 <?php
-//include 'vendor/autoload.php';
-//include 'settings2.php';/ กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
+include 'vendor/autoload.php';
+include 'settings2.php';/ กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
