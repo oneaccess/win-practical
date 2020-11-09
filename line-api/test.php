8f0c4eeb5223f6b53c9d87da20907988dcc63643
oneaccess <cisarese@hotmail.com>
2020-11-09 16:27:53 +0700

    Update test.php

--- line-api/test.php
+++ line-api/test.php
@@ -4,14 +4,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
  
-// include composer autoload
 require_once 'vendor/autoload.php';
  
-// การตั้งเกี่ยวกับ bot
+
 require_once 'settings.php';
  
-// กรณีมีการเชื่อมต่อกับฐานข้อมูล
-//require_once("dbconnect.php");
 
  require_once('vendor/LINE/LINEBot.php');
         require_once('vendor/LINE/LINEBot/Constant/ActionType.php');
