dd5b629d9b4d11936c331dd3bfeab50f7e4ceae5
oneaccess <cisarese@hotmail.com>
2020-11-17 11:22:12 +0700

    Update botpush.php

--- line/botpush.php
+++ line/botpush.php
@@ -1,32 +1,27 @@
 <?php
 
-
-
 require "vendor/autoload.php";
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
+use LINE\LINEBot\Message\MultipleMessages;
+use LINE\LINEBot\Message\RichMessage\Markup;
+error_reporting(-1);
+        $setting = require('bot_settings.php');
+        $channelId = $setting['channelId'];
+        $channelSecret = $setting['channelSecret'];
+        $channelMid = $setting['channelMid'];
+        $accessToken = $setting['accessToken'];
 
-$access_token = 'vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=';
-
-$channelSecret = '800277791d946cc4e2847fbe2b48578e';
-
-$pushID = 'Uf34420370c875a3540bd79f148c915c2';
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
-$content = file_get_contents('php://input');
- 
-// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
-$events = json_decode($content, true);
-if(!is_null($events)){
-    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-    $replyToken = $events['events'][0]['replyToken'];
-    $typeMessage = $events['events'][0]['message']['type'];
-    $userMessage = $events['events'][0]['message']['text'];
-    $userMessage = strtolower($userMessage);
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($userMessage);
-    //$response = $bot->pushMessage($pushID, $textMessageBuilder);
-    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-}
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+$config = [
+                        'channelId' => $channelId,
+                        'channelSecret' => $channelSecret,
+                        'channelMid' => $channelMid,
+                ];
 
 
+                $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+                $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+                $response = $bot->pushMessage($targetMid, $textMessageBuilder);
+                echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
