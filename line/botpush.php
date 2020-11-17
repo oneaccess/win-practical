0fcec8de70507762d05864545a2333066d66d2b5
oneaccess <cisarese@hotmail.com>
2020-11-17 11:50:44 +0700

    Update botpush.php

--- line/botpush.php
+++ line/botpush.php
@@ -16,6 +16,7 @@
         $channel_access_token = 'vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=';
         $channel_secret = '800277791d946cc4e2847fbe2b48578e';
         $to = 'U8ce89a9c5318ef4fbbeef105ca1bf201';
+        $userId = 'Uf34420370c875a3540bd79f148c915c2';
         $msg = 'hello';
 
          $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channel_access_token);
