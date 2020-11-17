b4e06cf4b307e66a18f4b33d4972276ce81e8630
oneaccess <cisarese@hotmail.com>
2020-11-17 11:47:24 +0700

    Update botpush.php

--- line/botpush.php
+++ line/botpush.php
@@ -1,17 +1,17 @@
 <?php
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/Constant/ActionType.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/Constant/EventSourceType.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/Constant/HTTPHeader.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/Constant/MessageType.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/Constant/ActionType.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/Constant/EventSourceType.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/Constant/HTTPHeader.php');
+        require_once('vendor/linecorp/line-bot-sdk//src/LINEBot/Constant/MessageType.php');
         require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/Constant/Meta.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/Constant/TemplateType.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/HTTPClient.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/HTTPClient/Curl.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/HTTPClient/CurlHTTPClient.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/MessageBuilder.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/MessageBuilder/TextMessageBuilder.php');
-        require_once('vendor/linecorp/line-bot-sdksrc/LINEBot/Response.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/Constant/TemplateType.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/HTTPClient.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/HTTPClient/Curl.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/HTTPClient/CurlHTTPClient.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/MessageBuilder.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/MessageBuilder/TextMessageBuilder.php');
+        require_once('vendor/linecorp/line-bot-sdk/src/LINEBot/Response.php');
 
         $channel_access_token = 'vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=';
         $channel_secret = '800277791d946cc4e2847fbe2b48578e';
