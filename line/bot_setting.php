ac7c5a2c2adbe05c3119f888e64d79b5c009c8ed
oneaccess <cisarese@hotmail.com>
2020-11-17 10:48:51 +0700

    Create bot_setting.php

---
+++ line/bot_setting.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1502306338');
+define('LINE_MESSAGE_CHANNEL_SECRET','800277791d946cc4e2847fbe2b48578e);
+define('LINE_MESSAGE_ACCESS_TOKEN','vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU=');
+?>
