ae86efeeecfb9db56790e5c885fad2b68e9ce51c
oneaccess <cisarese@hotmail.com>
2020-11-20 16:53:07 +0700

    Add files via upload

---
+++ line-api/bot_setting.php
@@ -0,0 +1,6 @@
+<?php
+# setting LINE Messageing API
+define('LINE_MESSAGE_CHANNEL_ID','1654880290'); 				// Channel ID
+define('LINE_MESSAGE_CHANNEL_SECRET','11244a6442f44298bc083b311d05e82f'); 	// Channel secret
+define('LINE_MESSAGE_ACCESS_TOKEN','h1e1F38wPPjkZq+1/xJWrZ1hhTdf0aH85fnVfyLFLjdzPDDj4ZvkNcQCltwFNu7e0bFhaNuK7JQCC1Zq6vczjMqfR2nddoIVTVi9unF5j6ZRZC2vdYirOkAUJcLR4cnjRqs+K9xDPp7GMt8au3/ReAdB04t89/1O/w1cDnyilFU='); // Channel access token
+?>
