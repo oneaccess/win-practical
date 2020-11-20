aa88dbec88ffefb0b3c712593cbc9365ecfad55f
oneaccess <cisarese@hotmail.com>
2020-11-20 20:28:53 +0700

    Update bot.php

--- line-api/bot.php
+++ line-api/bot.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-namespace LINE; 
+namespace LINE\LINEBot;; 
 
 // include composer autoload
 require_once 'vendor/autoload.php';
