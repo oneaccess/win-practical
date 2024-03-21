3429e1e3f61aa1c0d22f92e5a6240865b6c008a6
oneaccess <cisarese@hotmail.com>
2024-03-21 16:03:29 +0700

    Update bot_setting.php

--- line-api/bot_setting.php
+++ line-api/bot_setting.php
@@ -1,6 +1,5 @@
 <?php
 # setting LINE Messageing API
-define('LINE_MESSAGE_CHANNEL_ID','1502306338'); 				// Channel ID
-define('LINE_MESSAGE_CHANNEL_SECRET','800277791d946cc4e2847fbe2b48578e'); 	// Channel secret
-define('LINE_MESSAGE_ACCESS_TOKEN','vJkaEx7B0tYK6EtRxhYjBk70iDtSq5VYVT6+orl5AuqX82iChMQQUMyywaE2V5CNuY5dCRXrozUlssJQTWxxqpwj9lfXuF/IHWtttqi+HTQoiCj6tgc5Ijk+85l/Qdq2/z4llNHwMBh+11zXzJ1LAwdB04t89/1O/w1cDnyilFU='); // Channel access token
-?>
+define('LINE_MESSAGE_CHANNEL_ID','2004203680'); 				// Channel ID
+define('LINE_MESSAGE_CHANNEL_SECRET','ad150a90c8c3d8bbd57cd756addc0afd'); 	// Channel secret
+define('LINE_MESSAGE_ACCESS_TOKEN','DklEgicmMiL+M26w+PI6bARv9awjQPE9z1V6eGzRaCUN7YAt0Ec2NxUroQ1H+2A85hXpxquS690BqqpaD6UtTeVQ4P4CHIdnrPGoU/mFF02Jfpb/grpJP7Qz0IdXLMPeYwkGaR+1XhScgCDmAObPWgdB04t89/1O/w1cDnyilFU=');
